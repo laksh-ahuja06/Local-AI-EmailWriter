0c358907da727d0a731cf86364d781d08631843b
laksh-ahuja06 <ahuja.laksh@gmail.com>
2026-09-06 10:41:22 +0530

    Updated vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,6 @@ import tailwind from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VIT_BASE_PATH || "/Local-AI-EmailWriter",
+  base: "/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwind()],
 });
